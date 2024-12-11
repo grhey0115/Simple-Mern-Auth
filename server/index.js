5d9ea98942f135e76c46c7c654425478249b7573
grhey0115 <grhey0115@gmail.com>
2024-12-11 16:13:34 +0800

    check

--- server/index.js
+++ server/index.js
@@ -170,3 +170,9 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+if (process.env.VERCEL) {
+  console.log('Running on Vercel');
+} else {
+  console.log('Not running on Vercel');
+}
