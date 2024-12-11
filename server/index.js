4ba845817ced80aaca82fe0a333049dee7e767e6
grhey0115 <grhey0115@gmail.com>
2024-12-11 22:30:36 +0800

    add / on server

--- server/index.js
+++ server/index.js
@@ -170,3 +170,7 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the MERN Auth App!');
+});
